10e82b162c56096b8362e99860a03a4288720678
minhyeongKang <kang7937@gmail.com>
2023-11-30 10:47:05 +0900

    init commit

---
+++ src/main/java/com/sparta/todoparty/TodopartyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todoparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodopartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodopartyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/todoparty/TodopartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.todoparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodopartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
